8dccdaee5af4d793de5085f28910584da28308c6
Dante-Broggi <34220985+Dante-Broggi@users.noreply.github.com>
2023-10-11 16:12:58 -0400

    extract ChunkId

--- src/main.rs
+++ src/main.rs
@@ -160,49 +160,6 @@ impl std::error::Error for MyErr {
 
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub struct ChunkId(pub [u8; 4]);
-impl ChunkId {
-    const DATA_BIT: u8 = 32;
-    pub const IEND: Self = ChunkId(*b"IEND");
-
-    /// Each byte of a chunk type is restricted to
-    /// the hexadecimal values 41 to 5A and 61 to 7A.
-    pub fn is_valid(self) -> bool {
-        self.0.iter().all(|x| (0x41..=0x5A).contains(x) || (0x61..=0x7A).contains(x))
-    }
-
-    pub fn is_display_critical(self) -> bool {
-        (self.0[0] & Self::DATA_BIT) == 0
-    }
-    pub fn is_standard_defined(self) -> bool {
-        (self.0[1] & Self::DATA_BIT) == 0
-    }
-    pub fn is_reserved(self) -> bool {
-        (self.0[2] & Self::DATA_BIT) != 0
-    }
-    pub fn is_safe_to_copy(self) -> bool {
-        (self.0[3] & Self::DATA_BIT) != 0
-    }
-}
-impl Debug for ChunkId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // for c in self.0.map(|x| x as char) {
-        //     f.write_fmt(format_args!("{:?}", c))?;
-        // }
-        f.write_fmt(format_args!("{:?}", self.0.map(|x| x as char)))?;
-        Ok(())
-    }
-}
-impl Display for ChunkId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let iter = self.0.map(|x| x as char);
-        let s = String::from_iter(iter);
-        f.write_fmt(format_args!("{:?}", s))?;
-        Ok(())
-    }
-}
-
 fn parse_png_chunk_head(iter: &mut impl Iterator<Item = u8>) -> std::result::Result<(u32, ChunkId), MyErr> {
     let b = iter.next().ok_or(MyErr::EOF)?;
     let mut len: u32 = b.into();
@@ -231,7 +188,7 @@ fn parse_png_chunk_head(iter: &mut impl Iterator<Item = u8>) -> std::result::Res
 
 #[derive(Debug)]
 pub struct Chunk {
-    id: ChunkId,
+    id: chunk::ChunkId,
     data: Vec<u8>,
     crc: u32,
 }
