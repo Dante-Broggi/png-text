dcea26aa8a5716a0a03259a737710854af13a9e7
Dante-Broggi <34220985+Dante-Broggi@users.noreply.github.com>
2023-10-14 13:58:48 -0400

    fix compressed iTXt

--- src/chunk/itxt.rs
+++ src/chunk/itxt.rs
@@ -14,7 +14,8 @@ pub struct iTXt<'a> {
     // Null separator	1 byte (null character)
     trans: &'a str,
     // Null separator	1 byte (null character)
-    text: &'a str,
+    /// Not `str` if compressed:
+    text: &'a [u8],
 
 /*
 Keyword	1-79 bytes (character string)
@@ -63,7 +64,6 @@ impl<'a> iTXt<'a> {
         let (&n, chunk) = chunk.split_first().unwrap();
         assert_eq!(n, 0);
         let trans = core::str::from_utf8(trans).ok()?;
-        let text: &str = core::str::from_utf8(chunk).ok()?;
 
         Some(Self {
             keyword,
@@ -71,7 +71,7 @@ impl<'a> iTXt<'a> {
             cmthd,
             lang,
             trans,
-            text,
+            text: chunk,
         })
     }
 
@@ -83,16 +83,30 @@ impl<'a> iTXt<'a> {
 impl Display for iTXt<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if self.cflg == 0 {
-            if self.cmthd == 0 {
-                f.write_fmt(format_args!("{{id: {}, keyword: {:?}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
-                ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)),
-                String::from_iter(self.lang.into_iter().map(|&x| x as char)),
-                self.trans, self.text))
+            if let Ok(text) = core::str::from_utf8(self.text) {
+                if self.cmthd == 0 {
+                    f.write_fmt(format_args!("{{id: {}, keyword: {:?}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
+                    ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)),
+                    String::from_iter(self.lang.into_iter().map(|&x| x as char)),
+                    self.trans, text))
+                } else {
+                    f.write_fmt(format_args!("{{id: {}, keyword: {:?}, raw_cmthd: {}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
+                    ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)), self.cmthd,
+                    String::from_iter(self.lang.into_iter().map(|&x| x as char)),
+                    self.trans, text))
+                }
             } else {
-                f.write_fmt(format_args!("{{id: {}, keyword: {:?}, raw_cmthd: {}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
-                ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)), self.cmthd,
-                String::from_iter(self.lang.into_iter().map(|&x| x as char)),
-                self.trans, self.text))
+                if self.cmthd == 0 {
+                    f.write_fmt(format_args!("{{id: {}, keyword: {:?}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
+                    ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)),
+                    String::from_iter(self.lang.into_iter().map(|&x| x as char)),
+                    self.trans, self.text))
+                } else {
+                    f.write_fmt(format_args!("{{id: {}, keyword: {:?}, raw_cmthd: {}, lang: {:?}, trans_keyword: {:?}, text: {:?}}}",
+                    ChunkId::iTXt, String::from_iter(self.keyword.into_iter().map(|&x| x as char)), self.cmthd,
+                    String::from_iter(self.lang.into_iter().map(|&x| x as char)),
+                    self.trans, self.text))
+                }
             }
         } else {
             if self.cmthd == 0 {
